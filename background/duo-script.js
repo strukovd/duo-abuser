427e298d1be42c26d8db635657792b552f2c5bab
strukovd <dmitryi97@yandex.ru>
2022-12-29 02:34:09 +0600

    fixed bug and added logs

--- background/duo-script.js
+++ background/duo-script.js
@@ -74,6 +74,7 @@ class Abuser {
         console.log(`Вызван метод launchGame`);
         this.winCounter = 0;
 
+        console.log(`---> Добавляю три таски (старт и регистрация действий игры)`);
         this.tasks.push(()=>this.TASK_waitForLoadPage()); // Ждет когда страница загрузиться
         this.tasks.push(()=>this.TASK_clickNextButton("Далее")); // Нажимает кнопку далее
         this.tasks.push(()=>this.TASK_registerGameTasks()); // Регистрирует действия игры
@@ -86,6 +87,7 @@ class Abuser {
         console.log(`Вызван метод registerGameTasks`);
 
         // this.tasks = [];
+        console.log(`---> Добавляю 6 задач, для игры в след. раунд`);
         this.tasks.push(()=>this.TASK_waitForTextarea());
         this.tasks.push(()=>this.TASK_setAnswer());
         this.tasks.push(()=>this.TASK_clickNextButton("Проверить"));
@@ -105,16 +107,23 @@ class Abuser {
      */
     static TASK_clickNextButton(title) {
         console.log(`Вызван метод clickNextButton`);
+        console.log(`Параметры: title: ${title}`);
 
+        // Ищем кнопку
         const button = document.querySelector(`[data-test="player-next"]`);
         if(!button) return false;
         const buttonClassList = Array.from(button.classList).join(' ');
 
+        // Перебирает надписи на кпопке (если вдруг несколько span)
         for (const span of button.querySelectorAll(`span`)) {
+            // Если надпись кнопки совпадает с искомой
             if (span && span.textContent === title) {
+                console.log(`---> Нашел нужную кнопку, с надписью: "${title}", жму её`);
+
                 button.dispatchEvent(new Event('click', { bubbles: true }));
 
                 // Вынимаем тек. задачу, подкладываем проверку, и возвращаем тек. задачу в очередь
+                console.log(`---> Подкладываю задачу проверки нажатия isButtonSuccessfulPushed`);
                 const currentTask = this.tasks.shift();
                 this.tasks.unshift( ()=>this.isButtonSuccessfulPushed(title, buttonClassList) );
                 this.tasks.unshift(currentTask);
@@ -131,20 +140,48 @@ class Abuser {
      */
     static isButtonSuccessfulPushed(title, prevButtonClassList) {
         console.log(`Вызван метод isButtonSuccessful`);
+        console.log(`Параметры: title: ${title}`);
 
+        // Ищем кнопку
         const button = document.querySelector(`[data-test="player-next"]`);
-        if(!button) return true;
+        if(!button) {
+            console.log(`---> Кнопка не найдена! Видимо переход уже произошел.`);
+            return true;
+        }
         else {
             // Если кнопка есть и у нее такие же классы
             const curButtonClassList = Array.from(button.classList).join(' ');
             if(curButtonClassList === prevButtonClassList) {
                 // И такая же надпись, вероятно нажатие не сработало
+                console.log(`---> Кнопка (с теми же классами) найдена!`);
+                console.log(`---> Классы: ${curButtonClassList}`);
                 for (const span of button.querySelectorAll(`span`)) {
                     if (span && span.textContent === title) {
+                        console.log(`---> Кнопка имеет ту же надпись. Вероятно нажатие не сработало, жму еще раз!`);
+
+                        /*
+                        Иногда фронт Duo багует, и кнопка Далее скрывается, но не удаляется,
+                        из-за этого скрипт думает что кнока далее не нажалась и не завершает задачу,
+                        при это появляется экран завершения с кнопкой для выхода,
+                        внутри которой span с надписью "В другой раз", по нему и будем ориентироваться.
+                        След. блок проверяет наличие кнопки выхода, когда было уже 3 проигрыша и настал мод проигрыша.
+                        */
+                        if(Abuser.loseCounter === 3 && !Abuser.winMode) {
+                            const quitButton = document.querySelector(`[data-test="quit-button"]`);
+                            if( quitButton.querySelector(`span`) ) {
+                                console.log(`---> Произошел баг экрана, завершаем игру!`);
+                                return true;
+                            }
+                        }
+
                         button.dispatchEvent(new Event('click', { bubbles: true }));
                         return false;
                     }
                 }
+            }
+            else {
+                console.log(`---> Кнопка имеет другие классы, видимо это уже другая кнопка`);
+                console.log(`---> Классы: ${curButtonClassList}`);
             }
 
             return true;
@@ -156,6 +193,7 @@ class Abuser {
      */
     static TASK_waitForTextarea() {
         console.log(`Вызван метод waitForTextarea`);
+
         const textarea = document.querySelector(`[data-test="challenge-translate-input"]`);
         if (textarea) {
             return true;
@@ -170,24 +208,30 @@ class Abuser {
      */
     static TASK_setAnswer() {
         console.log(`Вызван метод setAnswer`);
+
         try {
             const question = Array.from( document.querySelectorAll(`[data-test=hint-token]`) ).map( (val )=> val.textContent ).join('')
             const textarea = document.querySelector(`[data-test="challenge-translate-input"]`);
+            if( !question || !textarea ) throw new Error("Что-то пошло не так..");
+
             let answer;
 
+            // Если включен режим победы
             if(this.winMode) {
+                // Ищем ответ
                 answer = this.answersMap[question];
+                // Если ответ не найден
                 if(answer === undefined) {
+                    console.log(`---> Ответ на вопрос: "${question}" не найден!`);
                     answer = 'asddsa';
                 }
             }
-            else {
+            else { // Если режим намеренного слива (проигрыша)
                 const randomDigit = Math.floor(Math.random() * 14);
-                answer = this.allowedStrings[randomDigit];
+                answer = this.allowedStrings[randomDigit]; // Берем ошибочную строку под ответ
             }
 
-            if( !question || !textarea ) throw new Error("Что-то пошло не так..");
-
+            console.log(`---> Вставляю ответ: ${answer}`);
             textarea.textContent = answer;
             textarea.dispatchEvent(new Event('input', { bubbles: true }));
 
@@ -205,6 +249,8 @@ class Abuser {
      */
     static TASK_checkAnswerStatus() {
         console.log(`Вызван метод countTheRound`);
+        // Этот метод вызывается на промежуточном экране, для определения того, был ли ответ верным
+
         try {
             const successClass = "_3e9O1";
             const failureClass = "_3vF5k";
@@ -247,10 +293,13 @@ class Abuser {
      * Меняет стутс игры если баллы получены
      */
     static TASK_checkIsGameEnded() {
+        console.log(`Вызван метод TASK_checkIsGameEnded`);
 
         if(!this.winMode && this.loseCounter === 3) {
             // 3 проигрыша
             // Нажать далее (при получении 20 баллов)
+
+            console.log(`---> Достигнуто 3 проигрыша (при проигрышном режиме), добавляю задачи: [нажать Далее] и [Аборт]`);
             const currentTask = this.tasks.shift();
             this.tasks = [];
             this.tasks.push(currentTask);
@@ -260,9 +309,14 @@ class Abuser {
         else {
             // Проверка того что текущее окно не получение 20 баллов
             // класс фиолетовой кнопки: .tEvKV
+
+            console.log(`---> 3 проигрыша не достигнуто (при ${this.winMode?"выйгрышном":"проигрышном"} режиме)`);
             if( document.querySelector(`.tEvKV`) ) {
+                console.log(`---> Обноружно окно получения 20ти баллов, включаю проигрышный режим`);
+
                 this.winMode = false;
                 // Нажать далее (при получении 20 баллов)
+                console.log(`---> Добавляю задачу [нажать Далее]`);
                 const currentTask = this.tasks.shift();
                 this.tasks.unshift( ()=>this.TASK_clickNextButton("Далее") );
                 this.tasks.unshift(currentTask);
